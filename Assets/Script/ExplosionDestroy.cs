5b7c626cd67e67cefc967bb49efdc1039328783e
neysaanggraini <neysa.anggraini.tik24@stu.pnj.ac.id>
2026-06-19 22:40:49 +0700

    Revisi 1

--- Assets/Script/ExplosionDestroy.cs
+++ Assets/Script/ExplosionDestroy.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class ExplosionDestroy : MonoBehaviour
 {
     public float destroyTime = 0.6f;
+    public AudioClip explosionSFX;
 
     void Start()
     {
+        // Mainkan suara ledakan
+        AudioSource.PlayClipAtPoint(
+            explosionSFX,
+            transform.position
+        );
+
+        // Hancurkan objek ledakan
         Destroy(gameObject, destroyTime);
     }
 }
